3743d544bdd9ef2aaa17ded207d94253cf48e3cb
github username <github email address>
2019-09-16 11:07:41 -0400

    Added an index controller and views for articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     def create 
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:notice] = "Article was successfully created."
             redirect_to article_path(@article)
         else
             # flash[:notice] = @article.errors.full_messages.to_sentence
@@ -23,6 +23,21 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
         render :show 
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully edited."
+            redirect_to article_path(@article)
+        else
+            render :edit 
+        end
     end
 
     private
